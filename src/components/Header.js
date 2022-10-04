e8a906587326c23d1a21a213147295ff0998908d
Rachel Mullen <mullen.rachel@gmail.com>
2022-10-04 11:07:47 -0600

    frontend updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Container from "@mui/material/Container";
 
 export default function Header({ user }) {
   return (
     <React.Fragment>
+      <Container>
       <header>
         <br />
         <br />
         <NavBar user={user} />
         <br />
         <br />
-      </header>
+        </header>
+      </Container>
     </React.Fragment>
   );
 }
