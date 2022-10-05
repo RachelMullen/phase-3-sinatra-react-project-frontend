68891bda34508f2591591bf5ab19d446a17813e1
Rachel Mullen <mullen.rachel@gmail.com>
2022-10-05 14:10:19 -0600

    frontend stuff

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,38 +9,32 @@ export default function NavBar({ user }) {
     <React.Fragment>
       <div className="navBar">
         <NavLink to="/create" button className='navLink'>
-            <AddCircle sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />{" "}
+            <AddCircle sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />
             CREATE
         </NavLink>
-        <NavLink to="/explore" className='navLink'>
-            <Explore sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />
-            EXPLORE
+
+        <NavLink to="/explore" button className='navLink'>
+        <Explore sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />
+          EXPLORE
         </NavLink>
+
         <img src={UpQuest} alt="Logo" width="200px"/>
-
-        <NavLink to="/go" className='navLink'>
-            <PinDrop sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />
-            GO
-        </NavLink>
-
-        <NavLink to="/explore">
-        <span className="navBar">EXPLORE</span>
-        </NavLink>
         
-        {/* <img className="main-logo" width="55px" src={Squirtle}></img> */}
-
-        <NavLink to="/go">
-        <span className ="navBar">GO</span>
+        <NavLink to="/go" button className='navLink'>
+        <PinDrop sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />
+          GO
         </NavLink>
 
         { !user ?
-        <NavLink to="/login">
-        <span className ="navBar">LOG IN</span>
+        <NavLink to="/login" button className='navLink'>
+            <Input sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />
+            LOGIN
         </NavLink>
         : null}
         { user ?
-        <NavLink to="/dashboard">
-        <span className ="navBar">DASHBOARD</span>
+        <NavLink to="/dashboard" button className='navLink'>
+            <Input sx={{ verticalAlign: "middle", paddingBottom: "3px" }} />
+            DASHBOARD
         </NavLink>
         : null}
       </div>
