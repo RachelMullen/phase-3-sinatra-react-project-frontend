422a75afcac219d1bb148a7e6f97cf574d169df6
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-06 12:57:45 -0600

    dashboard refresh redirects to home page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,20 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import Grid2 from "@mui/material/Unstable_Grid2";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import PaperContent from "@mui/material/CardContent";
-// import Checkbox from "@mui/material/Checkbox";
-import Container from "@mui/material/Container";
-import { Link } from "@mui/material";
+import { Redirect } from "react-router-dom";
 
 export default function Dashboard({ user }) {
 
   return (
     <React.Fragment>
-      <h1>{user[0].username}</h1>
-      <h1>Score: {user[3].stats.score}{user[3].stats.score > 0 ? ", nice." : ", you fucking idiot."}</h1>
+      {!user ?
+      <Redirect to="/" />
+      :
+      <>
+        <h1>{user[0].username}</h1>
+        <p>{user[0].locale}</p>
+        <p>{user[0].bio}</p>
+        <h1>Score: {user[3].stats.score}{user[3].stats.score > 0 ? ", nice." : ", you fucking idiot."}</h1>
+      </>
+      }
     </React.Fragment>
   );
 }
