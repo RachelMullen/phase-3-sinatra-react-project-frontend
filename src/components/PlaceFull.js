6b3846e17b074d20e5936100bbbfe2c8e7b14689
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-06 13:30:01 -0600

    visiting functionality in front end

--- src/components/PlaceFull.js
+++ src/components/PlaceFull.js
@@ -4,7 +4,7 @@ import Comment from "./Comment";
 
 
 export default function PlaceFull({ place }) {
-const [ isVisited, setIsVisited ] = useState(place[0].place.complete)
+const [ isVisited, setIsVisited ] = useState(place[1].linked_visit[0].complete)
 
     const comments = place[0].comments.map(comment => {
 
@@ -15,14 +15,30 @@ const [ isVisited, setIsVisited ] = useState(place[0].place.complete)
         )
     })
 
-    function handleVisit() {
+    // console.log(place[1].linked_visit[0].complete)
+    console.log(place[1].linked_visit[0])
 
+    function handleVisit() {
+        console.log("visit click")
+        setIsVisited(() => !isVisited)
+        let patchObj = {
+            complete: isVisited
+        }
+        fetch(`http://localhost:9292/${place[1].linked_visit[0].id}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(patchObj),
+          })
+          .then(resp => resp.json())
+          .then(data => console.log(data));
     }
 
     return (
         <>
             <h2 id="go-selected-place" style={{ fontSize: "2rem"}}>{place[0].place.name}</h2>
-            <button id="visit-button" style={{ color: "green"}} onClick={handleVisit}>{place[0].place.complete ? "UNVISIT" : "VISIT"}</button>
+            <button id="visit-button" style={{ color: "green"}} onClick={handleVisit}>{isVisited ? "VISITED" : "VISIT"}</button>
             <p id="go-selected-place-descrip">description: {place[0].place.description}</p>
             {/* <p>latitude: {place[0].place.latitude}</p>
             <p>longitude: {place[0].place.latitude}</p> */}
